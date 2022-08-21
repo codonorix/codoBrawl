4b74c99eb897fb9ab900ea8696f88507ce8c5f14
codonorix <logan.meadows20@gmail.com>
2022-08-21 15:37:08 +0200

    Initial commit

---
+++ src/main/java/za/co/codonorix/codobrawl/CodoBrawl.java
@@ -0,0 +1,17 @@
+package za.co.codonorix.codobrawl;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CodoBrawl extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		// Plugin startup logic
+		System.out.println("test");
+	}
+
+	@Override
+	public void onDisable() {
+		// Plugin shutdown logic
+	}
+}
