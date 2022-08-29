7382606c64e60ffb2a65c57bab5aa3b533fd3a78
codonorix <logan.meadows20@gmail.com>
2022-08-29 20:28:33 +0200

    First section of game mechanics (sign join)

--- src/main/java/za/co/codonorix/codobrawl/arena_game/SignJoinEvent.java
+++ src/main/java/za/co/codonorix/codobrawl/arena_game/SignJoinEvent.java
@@ -1,18 +1,41 @@
 package za.co.codonorix.codobrawl.arena_game;
 
+import net.kyori.adventure.text.TextComponent;
 import org.bukkit.block.Sign;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
+import za.co.codonorix.codobrawl.arena_creator.ArenaCreator;
+import za.co.codonorix.codobrawl.arena_creator.ArenaCreatorLoader;
+import za.co.codonorix.codobrawl.arena_creator.ArenaCreatorObject;
 
 public class SignJoinEvent implements Listener {
 	@EventHandler
 	public void onSignClick(PlayerInteractEvent event) {
-		event.getPlayer().sendMessage("test");
-		if(event.getClickedBlock() == null)return;
+		if(event.getClickedBlock() == null || event.getHand() == EquipmentSlot.OFF_HAND || event.getAction().isLeftClick())return;
 
-		if(event.getClickedBlock().getState() instanceof Sign ) {
-			event.getPlayer().sendMessage("Works!");
+		if(event.getClickedBlock().getState() instanceof Sign) {
+			ArenaCreatorObject gameArena = null;
+
+			Sign joinSign = (Sign) event.getClickedBlock().getState();
+			TextComponent signTitle = (TextComponent) joinSign.line(0);
+			TextComponent arenaName = (TextComponent) joinSign.line(1);
+
+			if(signTitle.content().equals("[CB]")){
+				for(int i = 0; i < ArenaCreatorLoader.arenas.size(); i++){
+					TextComponent arenaNamesList = (TextComponent) ArenaCreatorLoader.arenas.get(i).getName();
+					if(arenaName.content().equals(arenaNamesList.content())) {
+						gameArena = ArenaCreatorLoader.arenas.get(i);
+						break;
+					}
+				}
+
+				if(gameArena == null)return;
+
+				event.getPlayer().teleport(gameArena.getWaitingLobby());
+			}
+			//collosium
 		}
 	}
 }
